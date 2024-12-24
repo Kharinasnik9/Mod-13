2336d04530d135c934d6803b5f7520082ef4e153
Nikita Kharin <harin-1990@yandex.ru>
2024-12-24 20:07:50 +0400

    Добавьте файлы проекта.

---
+++ Task 1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string filePath = @"D:\input.txt";
+
+        string[] lines = File.ReadAllLines(filePath);
+
+        Console.WriteLine($"Всего строк в файле: {lines.Length}");
+
+        // Тестирование производительности для List<string>
+        TestPerformance(lines, new List<string>());
+
+        // Тестирование производительности для LinkedList<string>
+        TestPerformance(lines, new LinkedList<string>());
+    }
+
+    private static void TestPerformance(string[] lines, ICollection<string> collection)
+    {
+        var stopwatch = new Stopwatch();
+
+        stopwatch.Start();
+
+        foreach (var line in lines)
+        {
+            collection.Add(line);
+        }
+
+        stopwatch.Stop();
+
+        if (collection is List<string>)
+        {
+            Console.WriteLine("Время вставки в List<string>: " + stopwatch.ElapsedMilliseconds + " мс");
+        }
+        else if (collection is LinkedList<string>)
+        {
+            Console.WriteLine("Время вставки в LinkedList<string>: " + stopwatch.ElapsedMilliseconds + " мс");
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.IO;
+
+class WordFrequencyCounter
+{
+    static void Main()
+    {
+
+        string filePath = @"D:\input.txt";
+
+        try
+        {
+
+            string text = File.ReadAllText(filePath);
+
+
+            string[] words = Regex.Split(text, @"\W+")
+                                  .Where(w => !string.IsNullOrEmpty(w))
+                                  .Select(w => w.ToLower()) // Преобразуем каждое слово к нижнему регистру
+                                  .ToArray();
+
+
+            Dictionary<string, int> wordFrequencies = new Dictionary<string, int>();
+
+            // Подсчет частоты слов
+            foreach (string word in words)
+            {
+                if (!wordFrequencies.ContainsKey(word))
+                {
+                    wordFrequencies[word] = 0;
+                }
+                wordFrequencies[word]++;
+            }
+
+            // Сортируем слова по убыванию их частоты
+            var topWords = wordFrequencies.OrderByDescending(kvp => kvp.Value).Take(10);
+
+            // Выводим результат
+            Console.WriteLine("Топ-10 наиболее часто встречающихся слов:");
+            foreach (var item in topWords)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
